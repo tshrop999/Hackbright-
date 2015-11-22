1bd89386f7c06b216de0f51466ed313b82cc18de
tshrop999 <tshrop@alumni.stanford.edu>
2015-11-21 17:28:21 -0800

    Create calculator.py
    functions practice

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add(num1,num2):
+	return num1+num2
+print add(4,5)
+
+def subtract(num1,num2):
+	return num2-num1
+print subtract(4,5)
+
+def multiply(num1,num2):
+	return num1*num2
+print multiply(4,5)
+
+def divide(num1,num2):
+	return num2/num1
+print "%.4f" % divide(4,5)
+
+def modulo(num1,num2):
+	return num1%num2
+print modulo(4,5)
+
+def power(base,exponent):
+	return base**exponent
+print power(2,3)
+
+def square(num):
+	return num**2
+print square(3)
+
+add(4,5) + subtract(6,9)
