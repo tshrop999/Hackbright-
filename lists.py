ac15ddc0d389bbb8c52572a9e4722d41e66174be
tshrop999 <tshrop@alumni.stanford.edu>
2015-11-21 17:26:14 -0800

    list homework

---
+++ lists.py
@@ -0,0 +1,16 @@
+my_list  = [1, 2, "kitten", 4, "five", 6, 7]
+# print my_list
+
+# print my_list[2]
+# print my_list[3]
+# print my_list[1]
+
+print my_list[-1]
+print my_list[4]
+print my_list[(len(my_list)-1)]
+
+print len(my_list)
+
+print my_list[1:10]
+print my_list[-3:-1]
+
